6b3eef470eff063af3f36a89aeec71e8b84ca019
Christian Doucette <cdoucette@uchicago.edu>
2023-07-30 21:24:35 -0400

    allow autocomplete on all words in monster names, not just from start

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -22,25 +22,33 @@ function autocomplete(inp, arr) {
         /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
-        for (i = 0; i < arr.length; i++) {
-          /*check if the item starts with the same letters as the text field value:*/
-          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-            /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/
-            b.addEventListener("click", function(e) {
-                /*insert the value for the autocomplete text field:*/
-                inp.value = this.getElementsByTagName("input")[0].value;
-                /*close the list of autocompleted values,
-                (or any other open lists of autocompleted values:*/
-                closeAllLists();
-            });
-            a.appendChild(b);
+        for (const elt of arr) {
+          // get the indexes of the starts of each word
+          const whitespaceMatches = [...elt.matchAll(/(\s|\()/g)]
+          const wordStartIndexes = [0].concat(whitespaceMatches.map(match => match.index + 1))
+
+          for (const wordStartIndex of wordStartIndexes) { 
+
+            /*check if the item starts with the same letters as the text field value:*/
+            if (elt.substring(wordStartIndex, wordStartIndex + val.length).toUpperCase() == val.toUpperCase()) {
+              /*create a DIV element for each matching element:*/
+              b = document.createElement("DIV");
+              /*make the matching letters bold:*/
+              b.innerHTML = elt.substring(0, wordStartIndex)
+              b.innerHTML += "<strong>" + elt.substring(wordStartIndex, wordStartIndex + val.length) + "</strong>";
+              b.innerHTML += elt.substring(wordStartIndex + val.length);
+              /*insert a input field that will hold the current array item's value:*/
+              b.innerHTML += "<input type='hidden' value='" + elt + "'>";
+              /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+                  /*insert the value for the autocomplete text field:*/
+                  inp.value = this.getElementsByTagName("input")[0].value;
+                  /*close the list of autocompleted values,
+                  (or any other open lists of autocompleted values:*/
+                  closeAllLists();
+              });
+              a.appendChild(b);
+            }
           }
         }
     });

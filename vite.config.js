314fb4dd285b36cda48a2cad2698cf3a381d40cd
Christian Doucette <cdoucette@uchicago.edu>
2024-01-15 18:58:05 -0500

    Fix filename to unblock deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        selector: resolve(__dirname, "monster-selector.html"),
+        selector: resolve(__dirname, "choose-monster.html"),
       },
     },
   },

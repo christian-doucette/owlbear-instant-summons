5d841571e341ec3364860c849b39b78bf64bd5fd
Christian Doucette <cdoucette@uchicago.edu>
2024-01-15 15:11:32 -0500

    Refactor: move place monster functionality to its own file

--- src/create-tools.js
+++ src/create-tools.js
@@ -1,4 +1,6 @@
 import OBR, { buildImage } from "@owlbear-rodeo/sdk";
+import { placeCurrentMonster } from '/src/place-monsters.js';
+ 
 const ID = "monster-selector-tool";
 
 export function createInstantSummonsTool() {
@@ -22,42 +24,6 @@ export function createInstantSummonsTool() {
   });
 }
 
-function getOffset(pointerPositionX, pointerPositionY, size) {
-  const centeredX = round(pointerPositionX, 150.0, size);
-  const centeredY = round(pointerPositionY, 150.0, size);
-  return {
-    x: -2 * centeredX + (size / 2.0),
-    y: -2 * centeredY + (size / 2.0)
-  };
-}
-
-function round(coordinate, squareSize, tokenSize) {
-  if (tokenSize == 600.0 || tokenSize == 1200.0) {
-    const rounded = Math.round(coordinate / squareSize) * squareSize;
-    return rounded;
-  } else {
-    const rounded = Math.floor(coordinate / squareSize) * squareSize;
-    return rounded + (squareSize / 2.0);
-  }
-}
-
-function buildMonsterImage(imageUrl, size, pointerPositionX, pointerPositionY) {
-  return buildImage(
-    {
-      height: size,
-      width: size,
-      url: imageUrl,
-      mime: "image/png",
-    },
-    { 
-      dpi: 300, 
-      offset: getOffset(pointerPositionX, pointerPositionY, size)
-    }
-  )
-    .layer("CHARACTER")
-    .build();
-}
-
 export function createPlaceMonstersToolMode() {
   OBR.tool.createMode({
     id: `${ID}/mode`,
@@ -73,9 +39,7 @@ export function createPlaceMonstersToolMode() {
     preventDrag: { dragging: true },
     onToolClick: () => true,
     async onToolDoubleClick(_, event) {
-      const metadata = await OBR.tool.getMetadata(`${ID}/tool`);
-      const monsterImage = buildMonsterImage(metadata.url, metadata.size, event.pointerPosition.x, event.pointerPosition.y);
-      OBR.scene.items.addItems([monsterImage]);
+      placeCurrentMonster(event.pointerPosition)
     },
   });
 }
---
+++ src/place-monsters.js
@@ -0,0 +1,45 @@
+import OBR, { buildImage } from "@owlbear-rodeo/sdk";
+
+const ID = "monster-selector-tool";
+
+function round(coordinate, squareSize, tokenSize) {
+  if (tokenSize == 600.0 || tokenSize == 1200.0) {
+    const rounded = Math.round(coordinate / squareSize) * squareSize;
+    return rounded;
+  } else {
+    const rounded = Math.floor(coordinate / squareSize) * squareSize;
+    return rounded + (squareSize / 2.0);
+  }
+}
+
+function getOffset(pointerPositionX, pointerPositionY, size) {
+  const centeredX = round(pointerPositionX, 150.0, size);
+  const centeredY = round(pointerPositionY, 150.0, size);
+  return {
+    x: -2 * centeredX + (size / 2.0),
+    y: -2 * centeredY + (size / 2.0)
+  };
+}
+
+function buildMonsterImage(imageUrl, size, pointerPositionX, pointerPositionY) {
+  return buildImage(
+    {
+      height: size,
+      width: size,
+      url: imageUrl,
+      mime: "image/png",
+    },
+    { 
+      dpi: 300, 
+      offset: getOffset(pointerPositionX, pointerPositionY, size)
+    }
+  )
+    .layer("CHARACTER")
+    .build();
+}
+
+export async function placeCurrentMonster(pointerPosition) {
+  const metadata = await OBR.tool.getMetadata(`${ID}/tool`);
+  const monsterImage = buildMonsterImage(metadata.url, metadata.size, pointerPosition.x, pointerPosition.y);
+  OBR.scene.items.addItems([monsterImage]);
+}

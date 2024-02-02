5cdc6c81abc76542eb14b5e6b09bfc06c9a11cbc
Christian Doucette <cdoucette@uchicago.edu>
2024-02-02 18:17:48 -0500

    Add additional sources

--- src/choose-monster/monster-data.js
+++ src/choose-monster/monster-data.js
@@ -8,30 +8,25 @@ export async function allMonsterNames() {
     .then(json => Object.keys(json));
 }
 
-const monsterNotFoundData = {
-  url: 'https://5e.tools/img/MM/notrealmonster.png',
-  size: 'M'
-};
-
 function sanitizeName(name) {
   return name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
 
-function formatMonsterData(name, size) {
+function formatMonsterData(name, size, source) {
   return {
-    url: `https://5e.tools/img/MM/${sanitizeName(name)}.png`,
+    url: `https://5e.tools/img/${source}/${sanitizeName(name)}.png`,
     size: size
   };
 }
 
 export async function findMonster(name) {
-  var monsterSize = await fetchMonsterData()
+  var monsterData = await fetchMonsterData()
     .then(response => response.json())
     .then(json => json[name]);
 
-  if (monsterSize) {
-    return formatMonsterData(name, monsterSize);
+  if (monsterData) {
+    return formatMonsterData(name, monsterData.size, monsterData.source);
   } else {
-    return monsterNotFoundData;
+    return formatMonsterData('notrealmonster', 'M', 'MM');
   }
 }
